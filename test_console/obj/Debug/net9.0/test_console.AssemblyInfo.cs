be9c9fb8c9a8b4286735759f39729ce3e3a82abb
A7MED-FATHY <af6287107@gmail.com>
2026-06-17 01:12:52 +0300

    Add all project files

--- test_console/obj/Debug/net9.0/test_console.AssemblyInfo.cs
+++ test_console/obj/Debug/net9.0/test_console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0892ac65916f02ff4c144444e0d9545a00908c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
